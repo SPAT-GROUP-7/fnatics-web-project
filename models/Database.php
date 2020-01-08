6c166f660ee8704b8d69af318de854f5bd31e4a3
Liam <liamsutton1997@outlook.com>
2020-01-08 13:27:52 +0000

    Documented the Database Class

--- models/Database.php
+++ models/Database.php
@@ -6,6 +6,7 @@ class Database
     protected static $_dbInstance = null;
     protected $_dbHandle;
 
+    // Establish a connection to the DB
     public function __construct($username, $password, $host, $dbName) {
         try {
             $this->_dbHandle = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
@@ -15,6 +16,7 @@ class Database
         }
     }
 
+    // return an instance of the DB, if an instance is allready active use it, otherwise create a new instance
     public static function getInstance() {
         $username = "TEST";
         $password = "TEST";
@@ -28,10 +30,12 @@ class Database
         return self::$_dbInstance;
     }
 
+    // Return the current connection to the DB
     public function getConnection() {
         return $this->_dbHandle;
     }
 
+    // Close the connection to the DB and release the resources used
     public function destruct() {
         $this->_dbHandle = null;
     }
