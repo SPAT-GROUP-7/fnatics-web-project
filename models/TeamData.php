414920f10fc1568ca1a8af6562b923a8c8ec4a9d
stephenmccallion <stephenjmccallion@icloud.com>
2020-01-09 14:46:07 +0000

    TeamData set updated admin page and team UI added

--- models/TeamData.php
+++ models/TeamData.php
@@ -12,4 +12,68 @@ class TeamData
         $this->_dbInstance = Database::getInstance();
         $this->_dbHandle = $this->_dbInstance->getConnection();
     }
+
+    //Fetch team by ID
+    public function fetchTeam($teamID){
+        $statement = $this->_dbHandle->prepare("SELECT teamName, isBusy, firstName, lastName  FROM Teams, Users ORDER BY teamName DESC");
+        $statement->execute();
+        $this->_dbInstance->__destruct();
+
+        $dataSet = [];
+        while ($dbRow = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $dataSet[] = new Team($dbRow);
+        }
+        return $dataSet;
+    }
+
+    //Fetches all teams
+    public function fetchAllTeams(){
+        $statement = $this->_dbHandle->prepare("SELECT teamName, isBusy, firstName, lastName FROM Teams, Users");
+        $statement->execute();
+        $this->_dbInstance->__destruct();
+
+        $dataSet = [];
+        while ($dbRow = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $dataSet[] = new Team($dbRow);
+        }
+        return $dataSet;
+    }
+
+    //Creates a team
+    public function createTeam(){
+        $statement = $this->_dbHandle->prepare("INSERT INTO Teams WHERE ");
+        $statement->execute();
+        $this->_dbInstance->__destruct();
+
+        $dataSet = [];
+        while ($dbRow = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $dataSet[] = new Team($dbRow);
+        }
+        return $dataSet;
+    }
+
+    public function updateTeam(){
+        $statement = $this->_dbHandle->prepare("UPDATE Teams SET teamName, ");
+        $statement->execute();
+        $this->_dbInstance->__destruct();
+
+        $dataSet = [];
+        while ($dbRow = $statement->fetch()) {
+            $dataSet[] = new Team($dbRow);
+        }
+        return $dataSet;
+    }
+
+    public function delete($teamName){
+        $statement = $this->_dbHandle->prepare("DELETE * FROM Teams WHERE ");
+        $statement->execute();
+        $this->_dbInstance->__destruct();
+
+        $dataSet = [];
+        while ($dbRow = $statement->fetch()) {
+            $dataSet[] = new Team($dbRow);
+        }
+        return $dataSet;
+    }
+
 }
