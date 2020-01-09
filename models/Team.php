3b3005c5467626848c4a772db06a97ca32c0449f
Liam Sutton <liamsutton1997@outlook.com>
2020-01-09 12:47:30 +0000

    Created Team Model

--- models/Team.php
+++ models/Team.php
@@ -8,5 +8,56 @@
 
 class Team
 {
+    protected $_teamID, $_teamName, $_isBusy, $_dateCreated, $_dateLastUpdated;
+
+    public function __construct($dbRow)
+    {
+        $this->_teamID = $dbRow['teamID'];
+        $this->_teamName = $dbRow['teamName'];
+        $this->_isBusy = $dbRow['isBusy'];
+        $this->_dateCreated = $dbRow['dateCreated'];
+        $this->_dateLastUpdated = $dbRow['dateLastUpdated'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeamID()
+    {
+        return $this->_teamID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeamName()
+    {
+        return $this->_teamName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getisBusy()
+    {
+        return $this->_isBusy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateCreated()
+    {
+        return $this->_dateCreated;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateLastUpdated()
+    {
+        return $this->_dateLastUpdated;
+    }
+
 
 }
