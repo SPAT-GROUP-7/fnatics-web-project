02610036e03690391a166055ee9b00e823615b8c
Sutton Liam <sgb959@leto.cs.salford.ac.uk>
2020-01-09 12:31:47 +0000

    TeamData and UserData Classes now connect to the DB

--- models/TeamData.php
+++ models/TeamData.php
@@ -1,12 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sgb959
- * Date: 09/01/20
- * Time: 12:24
- */
+require_once ("Database.php");
+require_once ("Team.php");
 
 class TeamData
 {
+    protected  $_dbHandle, $_dbInstance;
 
+    // Establish a connection to the DB
+    public function __construct()
+    {
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getConnection();
+    }
 }
--- models/UserData.php
+++ models/UserData.php
@@ -1,7 +1,15 @@
 <?php
-
+require_once ("Database.php");
+require_once ("User.php");
 
 class UserData
 {
+    protected $_dbHandle, $_dbInstance;
 
+    // Establish a connection to the DB
+    public function __construct()
+    {
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getConnection();
+    }
 }
