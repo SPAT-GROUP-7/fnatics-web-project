efd6ef4068b0b64753fc8cd11db85b3a60c03b4a
Liam Sutton <liamsutton1997@outlook.com>
2020-01-10 14:16:54 +0000

    implemented Rota Model

---
+++ models/Rota.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sgb959
+ * Date: 10/01/20
+ * Time: 14:11
+ */
+
+class Rota
+{
+    protected $_from, $_to, $_devA, $_devB;
+    public function __construct($dbRow) {
+        $this->_from = $dbRow['from'];
+        $this->_to = $dbRow['to'];
+        $this->_devA = $dbRow['devA'];
+        $this->_devB = $dbRow['devB'];
+    }
+
+    public function getFrom() {
+        return $this->_from;
+    }
+
+    public function getTo(){
+        return $this->_to;
+    }
+
+    public function getDevA() {
+        return $this->_devA;
+    }
+
+    public function getDevB() {
+        return $this->_devB;
+    }
+
+    public function getDevs() {
+        return ['devA' => $this->_devA, 'devB' => $this->_devB];
+    }
+    
+}
