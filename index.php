0b4e0a21169a5033f11beef2aa45ec26ec9967d1
stephenmccallion <stephenjmccallion@icloud.com>
2020-01-10 00:29:34 +0000

    View teams created and edit and delete buttons added to UI

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 session_start();
 
 require_once ("models/UserData.php");
+require_once ("models/TeamData.php");
 
 $view = new stdClass();
 $view->title = "Rota System - Fanatics";
 
 $userData = new UserData();
 
+//Creates a Team data object
+$teamData = new TeamData();
+
 $view->users = $userData->getAllUsers();
+
+//Fetches all teams to view
+$view->teams = $teamData->fetchAllTeams();
+
 require_once("views/index.phtml");
