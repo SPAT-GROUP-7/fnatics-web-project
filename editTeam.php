08645d9b6ede36ea24afb8de264e20a998ca9241
stephenmccallion <stephenjmccallion@icloud.com>
2020-01-10 14:44:54 +0000

    View teams created and edit and delete buttons added to UI

--- editTeam.php
+++ editTeam.php
@@ -5,6 +5,13 @@ $id = $_GET['userID'];
 
 $teamData = new TeamData();
 
+if (isset($_POST['submit'])){
+
+    $teamName = htmlentities($_POST['teamName']);
+    $lastUpdate = htmlentities($_POST['lastUpdate']);
+    $isBusy = htmlentities($_POST['isBusy']);
+}
+
 $teamData->updateTeam($id);
 
 header("Location: index.php");
