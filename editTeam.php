409b48f1292e013f300a60b91cd559b31b8fb954
stephenmccallion <stephenjmccallion@icloud.com>
2020-01-11 21:00:15 +0000

    connected to editTeam.php and updated editUser.php controllers

--- editTeam.php
+++ editTeam.php
@@ -1,16 +1,13 @@
 <?php
 require_once ("models/UserData.php");
 
-//$id = $_GET['userID'];
+$id = $_GET['userID'];
 
 if (isset($_POST['submit'])){
-
     $teamName = htmlentities($_POST['teamName']);
-    $lastUpdate = htmlentities($_POST['lastUpdate']);
     $isBusy = htmlentities($_POST['isBusy']);
+    $teamData = new TeamData();
+    $teamData->updateTeam($id);
 }
-
-$teamData = new TeamData();
-$teamData->updateTeam($id);
 
 header("Location: index.php");
