84ee4ae041e16c96a76bbbcb38e4bee79b768fdc
Beaver420 <iiprobever@gmail.com>
2020-01-13 10:08:39 +0000

    Moved edit stuff to Admin Panel.

--- adminPanel.php
+++ adminPanel.php
@@ -1,6 +1,25 @@
 <?php
 session_start();
+
+require_once ("models/UserData.php");
+require_once ("models/TeamData.php");
+require_once("models/ScheduleData.php");
+
 $view = new stdClass();
 $view->title = "Admin Panel";
+$view->pageName = "admin";
+
+$userData = new UserData();
+
+//Creates a Team data object
+$teamData = new TeamData();
+
+// Creates a Schedule data object
+$rotaData = new ScheduleData();
+
+$view->users = $userData->getAllUsers();
+
+//Fetches all teams to view
+$view->teams = $teamData->fetchAllTeams();
 
 require_once ("views/adminPanel.phtml");
--- index.php
+++ index.php
@@ -1,25 +1,11 @@
 <?php
 session_start();
 
-require_once ("models/UserData.php");
-require_once ("models/TeamData.php");
-require_once("models/ScheduleData.php");
 
 $view = new stdClass();
 $view->title = "Schedule System - Fanatics";
 $view->pageName = "index";
 
-$userData = new UserData();
 
-//Creates a Team data object
-$teamData = new TeamData();
-
-// Creates a Schedule data object
-$rotaData = new ScheduleData();
-
-$view->users = $userData->getAllUsers();
-
-//Fetches all teams to view
-$view->teams = $teamData->fetchAllTeams();
 
 require_once("views/index.phtml");
