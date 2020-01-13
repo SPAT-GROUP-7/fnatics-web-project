80e76d37cddd009f3e034969f7f546debb34f55f
Liam <liamsutton1997@outlook.com>
2020-01-13 12:36:52 +0000

    Trying to use serialization as a method of publishing N schedules (BROKEN)

--- generateRota.php
+++ generateRota.php
@@ -5,5 +5,5 @@ session_start();
 $view = new stdClass();
 $rotaData = new ScheduleData();
 $view->title = "Generate new Schedule";
-$view->rotas = $rotaData->generateRotas("01-01-2020", 6);
+$view->rotas = $rotaData->generateRotas(date('d-m-Y'), $_POST['num']);
 require_once ("views/generateRota.phtml");
