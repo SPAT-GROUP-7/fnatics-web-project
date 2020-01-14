e8f60a6e6b0fc5bd2fe0237612bb0405690d0662
Beaver420 <iiprobever@gmail.com>
2020-01-14 13:47:22 +0000

    Small UI update

--- createUser.php
+++ createUser.php
@@ -1,9 +1,10 @@
 <?php
 require_once ("models/UserData.php");
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['firstName'])) {
 
     $userData = new UserData();
+    $output = '';
 
     $teamID = htmlentities($_POST['teamID']);
     $username = htmlentities($_POST['username']);
@@ -12,7 +13,15 @@ if (isset($_POST['submit'])) {
     $lastName = htmlentities($_POST['lastName']);
     $isAdmin = isset($_POST['isAdmin']) ? 1 : 0;
 
-    $userData->createUser($teamID, $username, $password, $firstName, $lastName, $isAdmin);
+    $emailExists = $userData->checkUsernameExists($username);
 
-    header("Location: index.php");
+    if ($emailExists) {
+        $userData->createUser($teamID, $username, $password, $firstName, $lastName, $isAdmin);
+    } else {
+        $output = '<div class="alert alert-danger" id="error-message" role="alert">
+                            <strong>Error:</strong> A user with that email already exists!
+                        </div>';
+    }
+
+    echo $output;
 }
