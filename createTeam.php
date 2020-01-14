dfd0473f91bdf4422714a22d0751d3ba0a0f8200
Beaver420 <iiprobever@gmail.com>
2020-01-14 14:58:16 +0000

    Small UI update

--- createTeam.php
+++ createTeam.php
@@ -1,14 +1,22 @@
 <?php
 require_once ("models/TeamData.php");
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['teamName'])) {
 
     $teamData = new TeamData();
+    $output = '';
 
     $_teamName = htmlentities($_POST['teamName']);
     $_isBusy = isset($_POST['isBusy']) ? 1 : 0;
 
 
-    $teamData->createTeam($_teamName, $_isBusy);
-    header("Location: index.php");
+    if ($teamData->checkTeamNameExists($_teamName)){
+        $teamData->createTeam($_teamName, $_isBusy);
+    } else {
+        $output = '<div class="alert alert-danger" id="error-message" role="alert">
+                            <strong>Error:</strong> A team with that name already exists!
+                        </div>';
+    }
+
+    echo $output;
 }
