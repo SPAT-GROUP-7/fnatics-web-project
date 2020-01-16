03a9d2292a258c1401e3b48e3bfa7ff230be7854
Liam Sutton <liamsutton1997@outlook.com>
2020-01-16 12:37:56 +0000

    UI inputs now added to allow users to manually filter schedule date ranges

--- rota.php
+++ rota.php
@@ -7,4 +7,11 @@ $view = new stdClass();
 $view->title = "Schedule View";
 $view->rotas = $rotaData->getRotas("2020-01-01", "2020-02-28");
 
+
+if (isset($_POST['submit'])) {
+    $from = $_POST['dateFrom'];
+    $to = $_POST['dateTo'];
+
+    $view->rotas = $rotaData->getRotas($from, $to);
+}
 require_once ("views/rota.phtml");
