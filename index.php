88c031bc7aaa844fb34ff4ddcf6950fd244034b3
Liam Sutton <liamsutton1997@outlook.com>
2020-01-16 14:17:45 +0000

    update

--- index.php
+++ index.php
@@ -6,8 +6,16 @@ require_once("models/ScheduleData.php");
 $view = new stdClass();
 $view->title = "Schedule System - Fanatics";
 $view->pageName = "index";
-
 $rotaData = new ScheduleData();
-$view->rotas = $rotaData->getAllRotas();
+if (isset($_SESSION['userID'])) {
+    //TODO: change this with test data so there is a user on the schedule that we can login with
+    $id = 129;
+//    $id = $_SESSION['userID'];
+    $view->rotas = $rotaData->getUserSchedules($id);
+} else {
+    $view->rotas = $rotaData->getAllRotas();
+}
+
+
 
 require_once("views/index.phtml");
