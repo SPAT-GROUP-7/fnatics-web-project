41cce6d076e94377fc26f63231ef73776f531def
Liam Sutton <liamsutton1997@outlook.com>
2020-01-16 14:29:31 +0000

    When logged in, all rotas mentioning the user will be displayed

--- index.php
+++ index.php
@@ -8,9 +8,7 @@ $view->title = "Schedule System - Fanatics";
 $view->pageName = "index";
 $rotaData = new ScheduleData();
 if (isset($_SESSION['userID'])) {
-    //TODO: change this with test data so there is a user on the schedule that we can login with
-    $id = 129;
-//    $id = $_SESSION['userID'];
+    $id = $_SESSION['userID'];
     $view->rotas = $rotaData->getUserSchedules($id);
 } else {
     $view->rotas = $rotaData->getAllRotas();
