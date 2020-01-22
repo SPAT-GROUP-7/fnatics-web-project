1439e3efd6bedc228ebe82da7b48c686f91ef0b2
Beaver420 <iiprobever@gmail.com>
2020-01-22 13:03:12 +0000

    permissions

--- rotaDisplay.php
+++ rotaDisplay.php
@@ -1,24 +1,30 @@
 <?php
-require_once ("models/TeamData.php");
-require_once ("models/ScheduleData.php");
-require_once ("models/UnavailableData.php");
 session_start();
+if (isset($_SESSION['isAdmin']) && ($_SESSION['isAdmin'] == true)) {
 
-$view = new stdClass();
-$view->title = "Some Test Thing";
+    require_once ("models/TeamData.php");
+    require_once ("models/ScheduleData.php");
+    require_once ("models/UnavailableData.php");
 
-$teamData = new TeamData();
-$scheduleData = new ScheduleData();
-$unaData = new UnavailableData();
-$view->teams = $teamData->fetchAllTeams();
+    $view = new stdClass();
+    $view->title = "Fanatics Rota Display";
 
-$view->members = [];
-foreach ($view->teams as $team) {
-    $members = $teamData->getTeamMembers($team->getTeamID());
-    $view->members[] = $members;
+    $teamData = new TeamData();
+    $scheduleData = new ScheduleData();
+    $unaData = new UnavailableData();
+    $view->teams = $teamData->fetchAllTeams();
+
+    $view->members = [];
+    foreach ($view->teams as $team) {
+        $members = $teamData->getTeamMembers($team->getTeamID());
+        $view->members[] = $members;
+    }
+
+    $view->schedules = $scheduleData->getAllRotas();
+    $view->unavailable = $unaData->getAllUnavailableUsers();
+
+    require_once("views/rotaDisplay.phtml");
+
+} else {
+    echo "You need to be an admin to view this page.";
 }
-
-$view->schedules = $scheduleData->getAllRotas();
-$view->unavailable = $unaData->getAllUnavailableUsers();
-
-require_once("views/rotaDisplay.phtml");
