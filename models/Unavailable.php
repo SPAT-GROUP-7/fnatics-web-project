8da66791ced87094437d4c198f0d349866d04c61
Liam <liamsutton1997@outlook.com>
2020-01-22 12:35:05 +0000

    Documented the Unavailable Model

--- models/Unavailable.php
+++ models/Unavailable.php
@@ -1,16 +1,32 @@
 <?php
 
 
+/**
+ * Class Unavailable
+ * This class is used to represent absent users
+ */
 class Unavailable implements JsonSerializable
 {
+    /**
+     * @var $unavailableID : the ID of the instance
+     */
+    /**
+     * @var $userID : the ID of the user who is unavailable
+     */
+    /**
+     * @var $teamID : the ID of the team which a user belongs to
+     */
+    /**
+     * @var $dateFrom : the start date of the absence
+     */
+    /**
+     * @var $dateTo : the end date of the absence
+     */
     protected $unavailableID, $userID, $teamID, $dateFrom, $dateTo;
 
     /**
+     * @param $dbRow : the database row containing the information to construct an Object
      * Unavailable constructor.
-     * @param $unavailableID
-     * @param $userID
-     * @param $teamID
-     * @param $dateFrom
      */
     public function __construct($dbRow)
     {
@@ -22,7 +38,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @return mixed
+     * @return mixed : get the ID of the instance
      */
     public function getUnavailableID()
     {
@@ -30,7 +46,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @param mixed $unavailableID
+     * @param mixed $unavailableID : the new ID of the instance
      */
     public function setUnavailableID($unavailableID): void
     {
@@ -38,7 +54,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @return mixed
+     * @return mixed : the ID of the User
      */
     public function getUserID()
     {
@@ -46,7 +62,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @param mixed $userID
+     * @param mixed $userID : the new ID of the User
      */
     public function setUserID($userID): void
     {
@@ -54,7 +70,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @return mixed
+     * @return mixed : the ID of the team
      */
     public function getTeamID()
     {
@@ -62,7 +78,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @param mixed $teamID
+     * @param mixed $teamID : the new ID of the team
      */
     public function setTeamID($teamID): void
     {
@@ -70,7 +86,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @return mixed
+     * @return mixed : the start date
      */
     public function getDateFrom()
     {
@@ -78,7 +94,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @param mixed $dateFrom
+     * @param mixed $dateFrom : the new start date
      */
     public function setDateFrom($dateFrom): void
     {
@@ -86,7 +102,7 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @return mixed
+     * @return mixed : the end date
      */
     public function getDateTo()
     {
@@ -94,13 +110,17 @@ class Unavailable implements JsonSerializable
     }
 
     /**
-     * @param mixed $dateTo
+     * @param mixed $dateTo : the new end date
      */
     public function setDateTo($dateTo): void
     {
         $this->dateTo = $dateTo;
     }
 
+    /** Allows the PHP object to be encoded in JSON
+     *
+     * @return array|mixed
+     */
     public function jsonSerialize()
     {
         return get_object_vars($this);
